464274aed8adf073e507740c071414a86a94b01e
Matthew Harris <matthew@biofare.org>
2016-11-07 16:16:52 -0800

    Let's get started
    It’s going down

---
+++ Project 1 - Counter/Counter/Counter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Counter
+//
+//  Created by Matthew Harris on 11/7/16.
+//  Copyright © 2016 Matthew Harris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
